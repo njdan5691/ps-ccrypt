4d219b2f01b185c72185f2c28068097d4dfbdf82
Dan <njdan.5691@gmail.com>
2019-02-19 11:11:13 -0500

    Create ps-ccrypt-pkg.el

---
+++ ps-ccrypt-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-                                                                     
+(define-package "ps-ccrypt" "0.1" "reading/writing/loading encryped files" '((cl-lib "0.6.1")) :authors '(("Peter Selinger" . "selinger@users.sourceforge.net")) :maintainer '("Peter Selinger" . "selinger@users.sourceforge.net"))
